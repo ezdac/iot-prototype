1801e348f00624c76629c92774b3688d2a316453
ezdac <maxdacjs@gmail.com>
2016-08-25 16:19:39 +0200

    add host-ip print

--- raideniotproto/consumer_main.py
+++ raideniotproto/consumer_main.py
@@ -1,10 +1,15 @@
 from consumer import PowerConsumerDummy, JSONRPCServer
+import netinterfaces as ni
 import gevent
 
+DEFAULT_INTERFACE_NAME = 'eth0'
 RPCPort = 4040
 
 def main():
     app = PowerConsumerDummy()
+    ni.ifaddresses(DEFAULT_INTERFACE_NAME)
+    host = ni.ifaddresses(DEFAULT_INTERFACE_NAME)[2][0]['addr']
+    print host
     rpc_server = JSONRPCServer(app, '127.0.0.1', RPCPort)
     rpc_server.start()
     # while not app.consumer_ready:
@@ -15,3 +20,10 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+    # For raspberry:
+    # sudo apt-get install build-essential automake pkg-config libtool libffi-dev libgmp-dev libssl-dev
+    # install solidity:
+
+
+# TODO: exlude all solc dependend stuff
